20fd5ecd0f88c012db1eb02c4516f1ef11a6a5ef
paposeco <13892562+paposeco@users.noreply.github.com>
2023-02-13 21:00:39 +0000

    delete pages are working

--- models/author.js
+++ models/author.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const AuthorSchema = new Schema({
-  first_name: {type: String, required: true, maxLength: 100},
-  family_name: {type: String, required: true, maxLength: 100},
-  date_of_birth: {type: Date},
-  date_of_death: {type: Date},
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
 });
 
 // virtual for full name
 
-AuthorSchema.virtual("name").get(function(){
+AuthorSchema.virtual("name").get(function() {
   // we will make sure to avoid errors when one name is missing
   let fullname = "";
   //this refers to the AuthorSchema
-  if(this.first_name && this.family_name){
+  if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
   }
-  if(!this.first_name || !this.family_name){
+  if (!this.first_name || !this.family_name) {
     fullname = "";
   }
   return fullname;
@@ -26,9 +26,25 @@ AuthorSchema.virtual("name").get(function(){
 
 // virtual for url
 
-AuthorSchema.virtual("url").get(function(){
+AuthorSchema.virtual("url").get(function() {
   // an id is created automatically when a schema is used
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("lifespan").get(function() {
+  let death = DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
+  let birth = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+  if (death === "Invalid DateTime") {
+    death = "";
+  }
+  if (birth === "Invalid DateTime") {
+    return "Unknown";
+  }
+  return birth + " - " + death;
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
